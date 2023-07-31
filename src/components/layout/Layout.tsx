4b0c1a783c1f00c059271336d8d66d8246b18a68
jang_yoonsu <shagrat8546@gmail.com>
2023-08-01 03:27:33 +0900

    Feat : 리다이렉트 분기처리 추가

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,11 +1,29 @@
 /** @jsxImportSource @emotion/react */
 
-import { ReactNode } from "react";
-import Header from "./Header";
+import { ReactNode, useEffect } from "react";
 import { colors } from "../../constants/color";
 import { Toaster } from "react-hot-toast";
+import { useLocation, useNavigate } from "react-router-dom";
+import { useRecoilValue } from "recoil";
+import { accessTokenState } from "../../store/recoilAtoms";
+
+import Header from "./Header";
 
 const Layout = ({ children }: { children: ReactNode }) => {
+  const location = useLocation();
+  const pathname = location.pathname;
+  const navigate = useNavigate();
+  const isLoggedin = useRecoilValue(accessTokenState);
+
+  useEffect(() => {
+    if (isLoggedin && ["/signup", "/signin"].includes(pathname)) {
+      navigate("/todo");
+    }
+    if (!isLoggedin && ["/todo"].includes(pathname)) {
+      navigate("/signin");
+    }
+  }, [pathname, isLoggedin, navigate]);
+
   return (
     <div
       css={{
@@ -26,31 +44,22 @@ const Layout = ({ children }: { children: ReactNode }) => {
           overflow: "hidden",
           width: "calc(100% - 40px)",
           maxWidth: "1000px",
+          maxHeight: "90vh",
+          height: "100%",
           margin: "0 auto",
-          minHeight: "90vh",
           background: "rgba(255, 255, 255, 0.4)",
           boxShadow: "0 8px 32px 0 rgba(31, 38, 135, 0.37)",
           backdropFilter: "blur(5px)",
           borderRadius: "20px",
           marginTop: "5vh",
-          padding: "100px 20px 20px 20px",
+          padding: "110px 30px 30px 30px",
         }}
       >
-        <Toaster
-          toastOptions={{
-            style: {
-              padding: "10px 16px",
-              fontSize: "14px",
-              fontWeight: 500,
-              color: colors.primary,
-            },
-          }}
-        />
         <Header />
         <main
           css={{
-            width: "100%",
             height: "100%",
+            width: "100%",
           }}
         >
           {children}
