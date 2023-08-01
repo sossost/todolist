e7c11b38e406a0e952f02df8c25212340bb164a8
윤수 <110542210+sossost@users.noreply.github.com>
2023-08-01 23:31:38 +0900

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ import TodoPage from "./pages/todo/TodoPage";
 
 function App() {
   return (
-    <Router basename="https://wanted-pre-onboarding-frontend-nu-eosin.vercel.app/">
+    <Router basename="/">
       <RecoilRoot>
         <Layout>
           <ToastProvider />
