99a644a0b6016e62a68fc5aeaed80b26ba1123fa
jang_yoonsu <shagrat8546@gmail.com>
2023-08-02 12:15:35 +0900

    Refactor : 투두리스트 데이터 불필요한 가공 삭제

--- src/api/todo.ts
+++ src/api/todo.ts
@@ -1,18 +1,9 @@
 import { Todo } from "../types";
 import { axiosBase } from "./axios";
 
-interface TodoResponse {
-  id: number;
-  todo: string;
-  isCompleted: boolean;
-  userId: number;
-}
-
 export const getAllTodos = async () => {
   const response = await axiosBase.get("/todos");
-  return response.data.map((todo: TodoResponse) => {
-    return { id: todo.id, todo: todo.todo, isCompleted: todo.isCompleted };
-  });
+  return response.data;
 };
 
 export const createTodo = async (todo: string) => {
@@ -21,11 +12,7 @@ export const createTodo = async (todo: string) => {
   return newTodo;
 };
 
-export const updateTodo = async (todo: {
-  id: number;
-  todo: string;
-  isCompleted: boolean;
-}) => {
+export const updateTodo = async (todo: Todo) => {
   const data = {
     todo: todo.todo,
     isCompleted: todo.isCompleted,
