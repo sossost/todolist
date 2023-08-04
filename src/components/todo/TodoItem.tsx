5700c79f2fc01e661e621a36c3ea252fdb88a589
jang_yoonsu <shagrat8546@gmail.com>
2023-08-04 18:06:48 +0900

    Design : 체크박스 디자인 변경

--- src/components/todo/TodoItem.tsx
+++ src/components/todo/TodoItem.tsx
@@ -18,10 +18,23 @@ import EditInput from "./EditInput";
 
 const TodoItem = ({ id, todo, isCompleted, userId }: Todo) => {
   const [isEditting, setIsEditting] = useState(false);
-  const [checked, setChecked] = useState(isCompleted);
   const [newTodo, setNewTodo] = useState(todo);
   const { isLoading, setIsLoading } = useContext(LoadingContext);
   const { todos: prevTodos, setTodos: setNewTodos } = useContext(TodoContext);
+
+  const handleCompleteTodo = async () => {
+    setIsLoading(true);
+    const newTodoData = { id, todo, isCompleted: !isCompleted, userId };
+    try {
+      await updateTodo(newTodoData);
+      const updatedTodos = updateClientTodos(prevTodos, id, newTodoData);
+      setNewTodos(updatedTodos);
+    } catch (error: any) {
+      toast.error(error.message);
+    } finally {
+      setIsLoading(false);
+    }
+  };
 
   const handleEditTodo = async () => {
     if (newTodo.trim().length === 0) {
@@ -85,11 +98,14 @@ const TodoItem = ({ id, todo, isCompleted, userId }: Todo) => {
   return (
     <TodoItemContainer>
       <TodoItemWrapper>
-        <input
+        <CheckBoxInput
           type="checkbox"
-          checked={checked}
-          onChange={() => setChecked(!checked)}
+          checked={isCompleted}
+          onChange={handleCompleteTodo}
         />
+        <Checkbox checked={isCompleted}>
+          {isCompleted && <CheckIcon src="/checkIcon.png" alt="check_icon" />}
+        </Checkbox>
         {isEditting ? (
           <EditInput
             id="modify-input"
@@ -98,7 +114,7 @@ const TodoItem = ({ id, todo, isCompleted, userId }: Todo) => {
             onChange={(e) => setNewTodo(e.target.value)}
           />
         ) : (
-          <TodoItemText>{todo}</TodoItemText>
+          <TodoItemText checked={isCompleted}>{todo}</TodoItemText>
         )}
       </TodoItemWrapper>
       <ButtonWrapper>
@@ -165,6 +181,31 @@ const TodoItemWrapper = styled.label`
   gap: 5px;
 `;
 
+const CheckBoxInput = styled.input`
+  position: absolute;
+  opacity: 0;
+  width: 0;
+  height: 0;
+`;
+
+const Checkbox = styled.span`
+  margin-top: 3px;
+  height: 18px;
+  aspect-ratio: 1;
+  border: 2px solid ${colors.primary};
+  border-radius: 9999px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: ${({ checked }: { checked: boolean }) =>
+    checked ? colors.primary : "transparent"}};
+`;
+
+const CheckIcon = styled.img`
+  width: 100%;
+  height: 100%;
+`;
+
 const TodoItemText = styled.span`
   box-sizing: content-box;
   font-size: 16px;
@@ -174,6 +215,8 @@ const TodoItemText = styled.span`
   line-height: 16px;
   padding: 8.3px 10px;
   color: ${colors.mainFont};
+  text-decoration: ${({ checked }: { checked: boolean }) =>
+    checked ? "line-through" : "none"};
 `;
 
 const ButtonWrapper = styled.div`
